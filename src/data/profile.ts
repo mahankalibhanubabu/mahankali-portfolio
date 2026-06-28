0faf620dbd30ae6e95eff91ce69e5426f3076966
mahankalibhanubabu <mahankalibhanubabu232323@gmail.com>
2026-06-28 07:08:15 -0400

    feat: add profile, experience, and project data constants

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -14,7 +14,7 @@ export const profile = {
   linkedin: "https://www.linkedin.com/in/mahankali-bhanubabu-devops-developer/",
   github: "https://github.com/mahankalibhanubabu",
   githubUser: "mahankalibhanubabu",
-  resumeUrl: "https://drive.google.com/file/d/1r_KABckrEDXT1y5obzh9foozhVI-MG6A/view?usp=sharing",
+  resumeUrl: "https://drive.google.com/file/d/19dCCg48IUOV_PYPGXwqn7PPShOX5q8vj/view?usp=sharing",
 } as const;
 
 export type ExperienceItem = {
