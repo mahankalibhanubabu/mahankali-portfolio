a0dcf3624d7ace2c2febc8ee306da347a979fc84
Mahankali Bhanubabu <139434040+mahankalibhanubabu@users.noreply.github.com>
2026-09-17 17:41:44 -0400

    Update resume URL in profile.ts

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -14,7 +14,7 @@ export const profile = {
   linkedin: "https://www.linkedin.com/in/mahankali-bhanubabu-devops-developer/",
   github: "https://github.com/mahankalibhanubabu",
   githubUser: "mahankalibhanubabu",
-  resumeUrl: "https://drive.google.com/file/d/1tnTOduazMlH5WyyaDkmAQmI93wzhDSmt/view?usp=sharing",
+  resumeUrl: "https://drive.google.com/file/d/1ZqMENwdyAM3Gdm15DYoXMSyHMN2maQZz/view?usp=sharing",
 } as const;
 
 export type ExperienceItem = {
